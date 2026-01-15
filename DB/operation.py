cee72b0df6f887b6d49b74d10ec3ad3862a2cce1
YiWei <b85125@gmail.com>
2026-01-15 20:35:09 +0800

    chore(db): clean up code

--- DB/operation.py
+++ DB/operation.py
@@ -5,12 +5,6 @@ from DB.testconnection import engine, Session, DB_URL
 CRUD與操作DB的各項函式
 """
 
-
 def create_table():
     Base.metadata.create_all(engine)
-    print("建表成功")
-
-def drop_table():
-    Base.metadata.drop_all(engine)
-    print('刪表成功')
-
+    print("創建jobs table成功")
