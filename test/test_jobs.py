103a1458f4e7b1a5b5226db84c0032b2881235f5
YiWei <b85125@gmail.com>
2026-01-15 16:32:05 +0800

    refactor(test): clean up test_job.py

--- test/test_jobs.py
+++ test/test_jobs.py
@@ -1,14 +1,14 @@
 from fastapi.testclient import TestClient
 from api.jobs import app
-from DB.models import Base
-from DB.testconnection import engine
-
-# CI 環境需要先建表
-Base.metadata.create_all(bind=engine)
 
 client = TestClient(app)
 
 def test_get_all_jobs():
+    """
+    1. CI測試連線是否成功
+    2. 測試API endpoint回傳值是否為list
+    """
+
     res = client.get("/jobs")
     assert res.status_code == 200
     assert isinstance(res.json(), list)
