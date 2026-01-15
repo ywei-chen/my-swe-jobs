56c7ebdf78673a27106f97f785ec00fd7ab1be6a
YiWei <b85125@gmail.com>
2026-01-15 16:48:47 +0800

    refactor(test): clean up test_job.py

--- test/test_jobs.py
+++ test/test_jobs.py
@@ -1,6 +1,9 @@
 from fastapi.testclient import TestClient
 from api.jobs import app
+from DB.models import Base
+from DB.testconnection import engine
 
+Base.metadata.create_all(bind=engine)
 client = TestClient(app)
 
 def test_get_all_jobs():
