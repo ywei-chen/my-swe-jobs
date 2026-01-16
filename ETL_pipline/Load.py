00c5ecb4ff4751413481648fcc627fb165724332
YiWei <b85125@gmail.com>
2026-01-16 20:16:28 +0800

    feat(etl): implenment job_id sequese

--- ETL_pipline/Load.py
+++ ETL_pipline/Load.py
@@ -1,17 +1,25 @@
 from DB.models import Job
 from DB.testconnection import Session
-
+from sqlalchemy import text
 
 def load_to_db(transformed_jobData, clear_data =True):
     """
     將trasform.py轉成dist後的資料轉成DB Model的格式
-    並存入PostgreSQL中
+    1. 先刪除舊資料
+    2. 重置PK(job_id)
+    3. 存入新資料到DB中
     """
     with Session() as db:
         try:
             if clear_data:
                 delete_count = db.query(Job).delete()
                 print(f"已刪除舊資料: 共{delete_count}筆")
+
+                restart_job_id = """
+                    ALTER SEQUENCE jobs_id_seq RESTART WITH 1
+                """
+                db.execute(text(restart_job_id))
+                print("已重置 job_id sequence")
 
             jobs = []
             for job_data in transformed_jobData:
